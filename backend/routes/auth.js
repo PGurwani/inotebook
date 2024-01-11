cecdcc457723b668755a3be0a1a75d05fd381f08
PGurwani <priyanshugurwani7385@gmail.com>
2024-01-11 17:35:53 +0530

    jwt token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
-
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const JWT_SECRET = 'Priyanshuisagoodb$oy';
 //Create a user using : POST "/api/auth/createuser"  No login required
 
 router.post('/createuser',[
@@ -25,18 +27,29 @@ router.post('/createuser',[
   {
     return res.status(400).json({error:"Sorry a user with this emailalready exists"})
   }
-
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
    user = await User.create
    ({
    name: req.body.name,
    email: req.body.email,
-   password:req.body.password,
+   password:secPass,
   })
   
   // .then(user=>res.json(user))
   // .catch(err=>{console.log(err)
   // res.json({error : 'PLease enter a unique value for email', message: err.message})})
-  res.json(user);
+  
+  // AB hum user nahi user ki jagah uski id bhejenge
+  const data = {
+    user : {
+      id : user.id
+    }
+  } 
+  const authtoken = jwt.sign(data, JWT_SECRET);
+  // console.log(jwtData);
+  // res.json(user)
+  res.json({authtoken});
   } catch(error)
   {
     console.error(error.message);
