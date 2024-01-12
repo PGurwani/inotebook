858ee8f420b26382e65e07a5452bd6db18e17b3f
PGurwani <priyanshugurwani7385@gmail.com>
2024-01-12 20:18:33 +0530

    Added Login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,7 +5,7 @@ const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 const JWT_SECRET = 'Priyanshuisagoodb$oy';
-//Create a user using : POST "/api/auth/createuser"  No login required
+//Route 1 ://Create a user using : POST "/api/auth/createuser"  No login required
 
 router.post('/createuser',[
    body('email', "Enter a valid email").isEmail(),
@@ -53,8 +53,45 @@ router.post('/createuser',[
   } catch(error)
   {
     console.error(error.message);
-    res.status(500).send("Some Error Occured");
+    res.status(500).send("INternal Server Error");
   }
 })
+//Route 2 ://Authenticate a user using : POST "/api/auth/createuser"  No login required
 
+router.post('/login',[
+  body('email', "Enter a valid email").isEmail(),
+  body('password', "Password cannot be blank").exists(),
+
+], async (req, res)=>{
+  //If there are errors retunr bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors : errors.array()});
+  }
+  const {email, password} = req.body;
+  try {
+    let user = await User.findOne({email});
+    if(!user){
+      return res.status(400).json({error : "PLease try to login with correct credentials"});
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if(!passwordCompare)
+    {
+      return res.status(400).json({error : "PLease try to login with correct credentials"});
+    }
+
+    const data = {
+      user : {
+        id : user.id
+      }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET);
+    res.json({authtoken})
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Some error Occured");
+  }
+ })
+
+ //Route 3 : Get loggin User Details using : POST "/api/auth/getuser" .Login required
 module.exports = router
