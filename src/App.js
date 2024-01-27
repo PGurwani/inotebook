2e399806e82bd384b54f39be5e5e615f4b31c3b7
PGurwani <priyanshugurwani7385@gmail.com>
2024-01-28 01:22:50 +0530

    Front end Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-
+import '../src/components/Navbar'
+import Navbar from '../src/components/Navbar';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Routeexact path="/">
+            <Home/>
+          </Routeexact>
+          <Route exact path="/about">
+            <About/>
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
